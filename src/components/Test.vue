ecad1d42723c2f1cddda498add7e03d6eef08408
Yang Xiaohong <610407721@qq.com>
2023-04-08 01:44:24 +0800

    恢复

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'PcIndex',
+  setup() {
+    alert('这里是PC');
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/view/PmdIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  这里是移动端
+</template>
+
+<script>
+export default {
+  name: 'PmdIndex',
+  setup() {
+    // alert('这里是移动端');
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
